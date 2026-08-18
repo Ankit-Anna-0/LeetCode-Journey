5809c9b8a5b9fb3d74c1de3fcec3ac022d729486
Ankit Anna <ankitanna03@gmail.com>
2026-08-18 14:51:59 +0530

    Create RomanToInteger.cpp

---
+++ LeetCode Solved Solution/RomanToInteger.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char,int> value={
+            {'I',1},
+            {'V',5},
+            {'X',10},
+            {'L',50},
+            {'C',100},
+            {'D',500},
+            {'M',1000}
+        };
+        int result = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (i+1<s.size() && value[s[i]]<value[s[i+1]])
+            {
+                result -= value[s[i]];
+            }
+            else
+            {
+                result += value[s[i]];
+            }
+            
+        }
+        return result;
+        
+    }
+};
+
+int main(){
+    Solution obj;
+
+    string s;
+    cin >> s;
+
+    int result = obj.romanToInt(s);
+    cout << result << endl;
+
+    return 0;
+}
