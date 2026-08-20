d495773bdaa66378ef3dc9ca87704fe78c4e2c9a
Ankit Anna <ankitanna03@gmail.com>
2026-08-20 08:10:17 +0530

    Add files via upload

---
+++ LeetCode Solved Solution/DistributeElementsIntoTwoArraysI.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1, arr2;
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back())
+                arr1.push_back(nums[i]);
+            else
+                arr2.push_back(nums[i]);
+        }
+
+        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+
+        return arr1;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    Solution s;
+    vector<int> result = s.resultArray(nums);
+
+    for(int x: result ){
+        cout << x << " ";
+    }
+
+    return 0;
+    
+}
