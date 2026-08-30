d0ea049b5b13fd1a8aefbee6ec0ed1a0a3a9b802
Ankit Anna <ankitanna03@gmail.com>
2026-08-30 18:33:18 +0530

    Add files via upload

---
+++ LeetCode Solved Solution/maxSubarraySum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 7;
+    int arr[7] = {3, -4, 5, 4, -1, 7,-8};
+
+    int maxSum = INT_MIN;
+
+    for (int srt = 0; srt < n; srt++)
+    {
+        int currSum = 0;
+        for (int end = srt; end < n; end++)
+        {
+           currSum += arr[end];
+           maxSum = max(currSum, maxSum);
+        }
+    }
+    cout << "max subarray sum = " << maxSum << endl;
+    return 0;
+    
+}
