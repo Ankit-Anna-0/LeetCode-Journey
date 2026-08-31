86b41ba646df1548d0b7303e96b9c15f669e9e3e
Ankit Anna <ankitanna03@gmail.com>
2026-08-31 22:58:44 +0530

    Add files via upload

---
+++ LeetCode Solved Solution/mooresVotingAlgo.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0, ans = 0;
+
+        // Find the candidate
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (freq == 0)
+            {
+                ans = nums[i];
+            }
+
+            if (ans == nums[i])
+            {
+                freq++;
+            }
+            else
+            {
+                freq--;
+            }
+        }
+
+        // Verify the candidate
+        int count = 0;
+
+        for (int val : nums)
+        {
+            if (val == ans)
+            {
+                count++;
+            }
+        }
+
+        if (count > nums.size() / 2)
+        {
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+
+    auto result = obj.majorityElement(arr);
+
+    cout << result;
+
+    return 0;
+}
